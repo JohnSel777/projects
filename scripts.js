9180223a81b9c627ed050b960cff97e062ea63a5
John Sel <64151964+JohnSel777@users.noreply.github.com>
2020-09-25 15:46:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,2 @@
-var x = document.querySelector("#form233065568 > div.t-form__inputsbox > div.t-input-group.t-input-group_nm.js-error-control-box > div > div");
-alert(x)
+var x = document.querySelector('.t-input-error');
+alert(x);
