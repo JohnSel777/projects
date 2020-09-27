0c1e5054c237df9517823a6fd6dd42ebe2c5c935
John Sel <64151964+JohnSel777@users.noreply.github.com>
2020-09-27 19:38:28 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,35 @@
-var err = document.querySelectorAll('.t-input-error')[0];
-err.innerHTML = "Введіть ваше ім'я";
+var div = document.getElementsByClassName('responds');
+var i;
+var divLen = div.length;
+for(x of div){
+      x.setAttribute('ontouchmove','touchMove(event)')
+  }
+var arr = [];
+function touchMove(event) {
+    var t = event.touches[0].clientX;
+    arr.push(t);
+    if(arr.length>2 && !(arr[0] == 0) && !(arr[1] == 0)){
+        arr.length = 2;
+        arr = [];
+    }
+console.log(arr);
+var delta = arr[1]-arr[0];
+  for(i=0; i<divLen; i++){
+        if(div[i] === event.currentTarget){
+            if(delta > 0){
+                div[i].classList.add('responds_transitioned_right')
+            }else if (delta < 0){
+                div[i].classList.add('responds_transitioned_left')
+                }
+            }
+        }
+    if(event.currentTarget === div[4]){
+        for(i = 0; i < 4; i++){
+            div[i].classList.remove('responds_transitioned_right','responds_transitioned_left')
+        }
+    }
+    else if(event.currentTarget === div[0]){
+        div[4].classList.remove('responds_transitioned_right','responds_transitioned_left');
+    }
+event.preventDefault();
+}
