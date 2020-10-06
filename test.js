b9ecbc32ea1378c36ddce56ef74624ac444c876a
John Sel <64151964+JohnSel777@users.noreply.github.com>
2020-10-06 10:53:15 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var x1 = document.querySelector("#form229969082 > div.t-form__inputsbox > div.t-input-group.t-input-group_nm > div > input");
 x1.placeholder = 'Ваше ім\'я';
-var x2 = document.querySelector("#form229969082 > div.t-form__inputsbox > div.t-input-group.t-input-group_nm.js-error-control-box > div > div");
+var x2 = document.querySelectorAll(".t-input-error")[0];
 x2.innerText = 'Введіть ваше ім\'я';
