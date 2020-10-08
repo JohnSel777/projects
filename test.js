d7eb27f488d69b5f6229aeffd41556685353352a
John Sel <64151964+JohnSel777@users.noreply.github.com>
2020-10-08 15:39:33 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,7 @@ var x1 = document.querySelector("#form229969082 > div.t-form__inputsbox > div.t-
 x1.placeholder = 'Ваше ім\'я';
 var x2 = document.querySelector("#form229969082 > div.t-form__inputsbox > div.t-input-group.t-input-group_nm > div > div");
 x2.innerText = 'Введіть ваше ім\'я';
+
+for (let button of document.querySelectorAll('button')){
+  button.setAttribute('class','buttons')
+};
